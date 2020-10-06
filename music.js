bbc81e40b156ee143c99053824af1d624215c48a
llooFlashooll <1330315641@qq.com>
2020-10-06 11:47:58 +0800

    Site updated: 2020-10-06 11:47:54

---
+++ music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    audio: [{
+        name: '囍',
+        artist: '葛东琪',
+        url: 'http://music.163.com/song/media/outer/url?id=1303289043.mp3',
+        cover: '',
+    }]
+});
